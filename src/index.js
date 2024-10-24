dfb322000fe60e3102478a52b8b7fa6e03ed0737
wataru <w.kajino2004@gmail.com>
2024-10-25 01:28:31 +0900

    check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 // src/index.js botの基本
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
+const { initCheck } = require('./features/reminder/initcheck');
+const { scheduleReminder } = require('./features/reminder/scheduler');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMessageReactions
+    ]
+});
+
+client.once(Events.ClientReady, c => {
+    console.log(`ログイン完了: ${c.user.tag}`);
+    initCheck(client);
+    scheduleReminder(client);
+});
+
+client.login(token).catch(error =>{
+    console.error(`Login failed: ${error}`);
+});
